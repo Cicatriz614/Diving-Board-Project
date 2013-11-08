b081a3f8b6b23148a6c777ae4fdc8452efaf1f7d
20440383 <antimatter@Live.ca>
2013-11-08 04:11:57 -0500

    Create CirTest.cpp

---
+++ CirTest.cpp
@@ -0,0 +1,63 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+
+#include "stdafx.h"
+
+#include <stdio.h>      /* printf, scanf, NULL */
+#include <stdlib.h>     /* malloc, free, rand */
+#include <iostream>
+#include <iomanip>
+
+#include <cmath>
+
+#include "CirMain.h"
+
+///*
+using namespace std;
+
+int main()
+{
+
+	CirMain* c_SSolve = new CirMain(10,50000000000);
+
+	
+	c_SSolve->Cin_PosIMtx();
+	c_SSolve->Cout_PosIMtx();
+
+	c_SSolve->Cin_DisMtx();
+	c_SSolve->Cout_DisMtx();
+	
+	c_SSolve->Trans_PosIF();
+	c_SSolve->Cout_PosFMtx();
+	
+	c_SSolve->Trans_PosLg();
+	c_SSolve->Cout_PosLMtx();
+
+	c_SSolve->Trans_PosAg();
+	c_SSolve->Cout_PosAMtx();
+	
+	c_SSolve->Trans_Multip();
+	c_SSolve->Cout_DisLMtx();
+
+	c_SSolve->Stiff_MultipHut();
+	c_SSolve->Cout_ForLMtx();
+	c_SSolve->Cout_StsTMtx();
+	
+	
+
+	c_SSolve->Cout_StsAMtx();	
+	c_SSolve->Cout_StsBMtx();
+
+	c_SSolve->Trans_StsTMtx();	
+	c_SSolve->Cout_StsTMtx();
+
+//	c_SSolve->Stiff_Multip204();
+//	c_SSolve->Cout_ForMtx();
+
+//	c_SSolve->Cout_StsBMtx();
+
+
+	system ("pause");
+	return 0;
+}
+
+
